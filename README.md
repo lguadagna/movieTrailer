6aa9ac99983b1ee10dba1875dbdd214526924a49
Lisa Guadanga <lisa.guadagna@google.com>
2016-11-20 18:49:02 -0800

    modified:   README.md 	modified:   entertainment_center.py

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-#MOvieTrailer
+#MovieTrailer
+
+##Description --
+This project creates a web page to display movie pictues.
+The page is generated with a list of movie objects. Each movie
+includes movie title, a brief description, and shows the movie trailer
+when the movie image is selected
+
+#Installation --
+Download the resository.
+if you have git installed: 
+git clone https://github.com/lguadagna/movieTrailer
+
+or use git hub "clone or download" button.
+
+run the file "_entertainment_center.py_"
+
+The program should open a browser window displaying the movies html file. 
+
+##Usage --
+
+You must have python installed. File extentions .py must be configured to run
+python programs
+
+You must have a default web browser configured. 
+
+##Tests --
+Select any movie after the browser window is opened. It should show a trailer. 
+
+
+##License --
+MITLicense.txt
+Original source of fresh_tomatoes.py file on github site:
+https://github.com/adarsh0806/ud036_StarterCode
--- entertainment_center.py
+++ entertainment_center.py
@@ -2,7 +2,8 @@ import media
 import fresh_tomatoes
 
 """
-The class entertainment_center (Entertainment Center) defines a list of movie objects and their attributes.
+The class entertainment_center (Entertainment Center) defines a list "
+" of movie objects and their attributes.
 The moveis are put into a list and sent to a web page constructor.
 """
 
@@ -16,19 +17,26 @@ toy_story = media.Movie("Toy Story",
 
 # Guardian of the Galaxy
 avatar = media.Movie("Guardians of the Galaxy",
-                    "A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe. ",
+                    "A group of intergalactic criminals are forced to work "
+                    "together to stop a fanatical warrior from taking control "
+                    "of the universe. ",
                     "guardians.jpg", 
                     "https://www.youtube.com/watch?v=d96cjJhvlMA")
 
 # Gone with the Wind
 wind = media.Movie("Gone with the Wind",
-                   "Epic Civil War drama focuses on the life of petulant southern belle Scarlett O'Hara. Starting with her idyllic on a sprawling plantation, the film traces her survival through the tragic history of the South during the Civil War and Reconstruction.",
+                   "Epic Civil War drama focuses on the life of petulant "
+                   "southern belle Scarlett O'Hara. Starting with her idyllic "
+                   " on a sprawling plantation, the film traces her survival "
+                   "through the tragic history of the South during the Civil "
+                   "War and Reconstruction.",
                    "http://www.dvdsreleasedates.com/covers/gone-with-the-wind-dvd-cover-63.jpg",
                     "https://www.youtube.com/watch?v=0dTsfsr6-X8")
 
 # Eternal Sunshine Movie information 
 sunshine = media.Movie("Eternal Sunshine of the Spotless Mind",
-                   "Relationships are built from memories both good and bad.Erasing them proves difficult",
+                   "Relationships are built from memories both good and bad. "
+                   "Erasing them proves difficult",
                    "eternal_sunshine.jpg",
                     "https://www.youtube.com/watch?v=rb9a00bXf-U")
 
